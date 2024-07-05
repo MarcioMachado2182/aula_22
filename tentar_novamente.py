fb6a9d753f235be0ab4dae5718f8ec91d2833b76
MárcioMachado2182 <marciojjb@gmail.com>
2024-07-05 17:19:45 -0300

    ultimo de hoje

--- tentar_novamente.py
+++ tentar_novamente.py
@@ -11,22 +11,28 @@ text.pack()
 
 label = tk.Label(janela)
 label.pack()
+
+botao_tentar_novamente = tk.Button(janela)
+botao_tentar_novamente.pack()
+botao_sair = tk.Button(janela)
+botao_sair.pack()
+
 def verificar_tamanho(event):
     tamanho = text.get('1.0', '2.0')
     if len (tamanho) > 2:
         text['state'] = 'disable'
-        #print('exedeu limite')
+       
         #label.config(text="Exedeu Limite", bg='purple', fg='silver', font = 'Arial 10')
-        botao_vermelho = tk.Button(janela, text="Clique para tentar novamente",bg='blue', fg='white')
-        botao_vermelho.pack()
+      
       
     else:
         text['state'] = 'normal'
 
 
 
-#def zerar_texto(event): 
-   
+def zerar_texto(event): 
+    botao_tentar_novamente.config(janela, text="Clique para tentar novamente",bg='blue', fg='white')
+    botao_sair.config(janela, text="Clique para sair",bg='red', fg='white')
 
 
 janela.bind('<KeyRelease>', verificar_tamanho)
